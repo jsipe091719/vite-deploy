b982a5d31b771164c57cf90280ab32197ff9c540
jsipe091719 <molhw202000072@wmsu.edu.ph>
2023-06-20 04:24:03 +0700

    Modified

---
+++ src/components/About.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function About() {
+  return <div name="about"></div>;
+}
+
+export default About;
---
+++ src/components/Feature.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Feature() {
+  return <div name="team"></div>;
+}
+
+export default Feature;
