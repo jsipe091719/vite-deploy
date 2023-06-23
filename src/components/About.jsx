edd12373b9e6902714b487ccc17160702323e977
jsipe091719 <molhw202000072@wmsu.edu.ph>
2023-06-23 07:56:06 +0700

    commit-changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -20,35 +20,14 @@ const About = () => {
             <div className="border border-gray-300 p-6 mt-8 md:mt-12 mx-auto w-full max-w-[800px] lg:max-w-[90%]">
               <p className="text-sm text-justify text-gray-800">
                 Welcome to Molave, a vibrant municipality nestled in the heart
-                of Zamboanga del Sur, Philippines. Makuguihon, formerly known as
-                Poblacion, is a charming barangay within Molave, enchanting
-                visitors with its rich history and warm hospitality. With a
-                population of 4,121 as of the 2020 Census, Makuguihon accounts
-                for 7.75% of the total population of Molave. Molave is a place
-                where natural beauty meets cultural heritage. The town is
-                blessed with picturesque landscapes, including rolling hills,
-                lush forests, and crystal-clear rivers. If you're an outdoor
-                enthusiast, you'll find plenty of opportunities for adventure,
-                from hiking through scenic trails to exploring the captivating
-                caves that dot the area. One of the highlights is the majestic
-                Mount Timolan, which offers breathtaking panoramic views from
-                its summit, making it a popular destination for hikers and
-                nature lovers. Beyond its natural wonders, Molave also boasts a
-                vibrant cultural scene. The town is known for its colorful
-                festivals, such as the Molave Town Fiesta, where locals and
-                visitors come together to celebrate with lively parades,
-                traditional dances, and sumptuous feasts. The warm and friendly
-                residents take great pride in preserving their heritage, and
-                you'll often find them showcasing traditional arts and crafts,
-                like the intricate woven products made from locally sourced
-                materials. Makuguihon, with its close-knit community and rich
-                cultural heritage, is the epitome of the warm spirit that
-                permeates Molave. Whether you're seeking an escape to nature or
-                an immersion into the local culture, Molave and Makuguihon offer
-                an unforgettable experience that will leave you longing to
-                return. Come and discover the beauty and charm of this hidden
-                gem in Zamboanga del Sur, where every visit is an opportunity to
-                create lasting memories.
+                of Zamboanga del Sur. Makuguihon, with its close-knit community
+                and rich cultural heritage, is the epitome of the warm spirit
+                that permeates Molave. Whether you're seeking an escape to
+                nature or an immersion into the local culture, Molave and
+                Makuguihon offer an unforgettable experience that will leave you
+                longing to return. Come and discover the beauty and charm of
+                this hidden gem in Zamboanga del Sur, where every visit is an
+                opportunity to create lasting memories.
               </p>
             </div>
           </div>
